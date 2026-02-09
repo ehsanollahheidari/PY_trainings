3a0c31bc74caf656045d002f238a9767f74a628f
Ehsan <ehsanheidari46@gmail.com>
2026-02-09 23:37:50 +0330

    Update Random Numbers.py

--- Random Numbers.py
+++ Random Numbers.py
@@ -1,6 +1,7 @@
 import random
 
 total_score = 0
+
 for level in range (1,4) :
      print (f"\n level {level} :")
 
@@ -20,4 +21,5 @@ for level in range (1,4) :
      else :
          print (f"Your chance is over! the correct number is {secret_number}!")
 print (f"\n Your total score is {total_score}")
+
 print ("End of the game!")
