8acf806b74e9207db9703d630938a2f34360cf6b
Ehsan <ehsanheidari46@gmail.com>
2026-02-09 23:42:25 +0330

    Update Random Numbers.py

--- Random Numbers.py
+++ Random Numbers.py
@@ -15,11 +15,13 @@ for level in range (1,4) :
              total_score += 10
              break
          elif guess > secret_number :
-             print ("this number is smaller than your guess!")
+              print ("this number is smaller than your guess!")
          elif guess < secret_number :
-             print ("this number is bigger than your guess!")
-     else :
-         print (f"Your chance is over! the correct number is {secret_number}!")
-print (f"\n Your total score is {total_score}")
+              print ("this number is bigger than your guess!")
+          else :
+               print (f"Your chance is over! the correct number is {secret_number}!")
 
+
+print (f"\n Your total score is {total_score}")
 print ("End of the game!")
+
