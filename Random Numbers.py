98d4e0515816bd779ab81f227b5b5de40b84448f
Ehsan <ehsanheidari46@gmail.com>
2026-02-09 23:45:00 +0330

    Update Random Numbers.py

--- Random Numbers.py
+++ Random Numbers.py
@@ -18,11 +18,12 @@ for level in range (1,4) :
               print ("this number is smaller than your guess!")
          elif guess < secret_number :
               print ("this number is bigger than your guess!")
-          else :
-               print (f"Your chance is over! the correct number is {secret_number}!")
+         else :
+              print (f"Your chance is over! the correct number is {secret_number}!")
 
 
 print (f"\n Your total score is {total_score}")
 print ("End of the game!")
+
 
 
